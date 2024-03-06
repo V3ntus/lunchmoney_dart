d0d311f537e7f498270f40cfbd74e47fecab750b
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-03-06 11:43:47 -0600

    feat: implement tag model

---
+++ lib/src/models/tag.dart
@@ -0,0 +1,33 @@
+import 'package:lunchmoney/src/models/_base.dart';
+
+class Tag implements LunchMoneyModel {
+  /// Unique identifier for tag.
+  @override
+  final int id;
+
+  /// User-defined name of tag.
+  final String name;
+
+  /// User-defined description of tag.
+  final String? description;
+
+  /// Whether the tag will not show up when creating or updating transactions
+  /// in the Lunch Money app.
+  final bool archived;
+
+  /// Construct a [Tag] object.
+  Tag({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.archived,
+  });
+
+  /// Construct a [Tag] from [data].
+  static fromJson(Map<String, dynamic> data) => Tag(
+        id: int.parse(data["id"]),
+        name: data["name"],
+        description: data["description"],
+        archived: data["archived"],
+      );
+}
