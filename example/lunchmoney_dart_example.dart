5ea25c86aac8d469d446833fd8a510f67e5de609
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-03-06 22:21:03 -0600

    ref: update example

--- example/lunchmoney_dart_example.dart
+++ example/lunchmoney_dart_example.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
 import 'package:lunchmoney/lunchmoney.dart';
 
-void main() {}
+void main() async {
+  final env = Platform.environment;
+
+  final client = LunchMoney(env["API_KEY"]!);
+
+  print((await client.user.me).userName);
+}
