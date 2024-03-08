3a7d03c726936adc004179e36e088f30c4fec6c5
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-03-08 08:35:03 -0600

    ref: instantiate routes in client

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,6 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:lunchmoney/src/api/http.dart';
 import 'package:lunchmoney/src/api/routes/asset.dart';
+import 'package:lunchmoney/src/api/routes/budget.dart';
+import 'package:lunchmoney/src/api/routes/category.dart';
+import 'package:lunchmoney/src/api/routes/crypto.dart';
+import 'package:lunchmoney/src/api/routes/plaid.dart';
+import 'package:lunchmoney/src/api/routes/recurring_expense.dart';
+import 'package:lunchmoney/src/api/routes/tags.dart';
+import 'package:lunchmoney/src/api/routes/transactions.dart';
 import 'package:lunchmoney/src/api/routes/user.dart';
 
 /// The main Lunch Money API client class.
@@ -11,22 +18,64 @@ class LunchMoney {
   /// be more convenient for users, a pull request is welcome.
   late final HTTPClient http;
 
-  late final UserRoute _user;
-
   late final AssetRoute _asset;
 
-  /// Make requests on the [UserRoute] user endpoint
-  UserRoute get user => _user;
+  late final BudgetRoute _budget;
+
+  late final CategoryRoute _category;
+
+  late final CryptoRoute _crypto;
+
+  late final PlaidRoute _plaid;
+
+  late final RecurringExpensesRoute _recurringExpenses;
+
+  late final TagsRoute _tags;
+
+  late final TransactionsRoute _transactions;
+
+  late final UserRoute _user;
 
   /// Make requests on the [AssetRoute] assets endpoint
   AssetRoute get assets => _asset;
+
+  /// Make requests on the [BudgetRoute] budgets endpoint
+  BudgetRoute get budgets => _budget;
+
+  /// Make requests on the [CategoryRoute] categories endpoint
+  CategoryRoute get categories => _category;
+
+  /// Make requests on the [CryptoRoute] crypto endpoint
+  CryptoRoute get crypto => _crypto;
+
+  /// Make requests on the [PlaidRoute] Plaid endpoints
+  PlaidRoute get plaid => _plaid;
+
+  /// Make requests on the [RecurringExpensesRoute] recurring expenses endpoints
+  RecurringExpensesRoute get recurring => _recurringExpenses;
+
+  /// Make requests on the [TagsRoute] tags endpoints
+  TagsRoute get tags => _tags;
+
+  /// Make requests on the [TransactionsRoute] transactions endpoints
+  TransactionsRoute get transactions => _transactions;
+
+  /// Make requests on the [UserRoute] user endpoint
+  UserRoute get user => _user;
 
   /// Initialize the Lunch Money API client with your access token provided.
   LunchMoney(String accessToken) {
     http = HTTPClient(accessToken: accessToken);
 
     // Instantiate routes
-    _user = UserRoute(this);
     _asset = AssetRoute(this);
+    _budget = BudgetRoute(this);
+    _category = CategoryRoute(this);
+    _crypto = CryptoRoute(this);
+    _plaid = PlaidRoute(this);
+    _recurringExpenses = RecurringExpensesRoute(this);
+    _tags = TagsRoute(this);
+    _transactions = TransactionsRoute(this);
+    _user = UserRoute(this);
   }
 }
