eb47c7b30d327c75f1b64ada680bfb1b911d3de6
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-03-10 22:08:08 -0500

    ref: add budgets to example

--- example/lunchmoney_dart_example.dart
+++ example/lunchmoney_dart_example.dart
@@ -9,4 +9,7 @@ void main() async {
 
   final transactions = await client.transactions.transactions(startDate: DateTime(2024, 2, 1), endDate: DateTime.now());
   print(transactions.map((e) => "${e.payee} -> ${e.amount}").join("\n"));
+
+  final budgets = await client.budgets.budgets(DateTime.now().copyWith(day: 1), DateTime.now().copyWith(day: 31));
+  print(budgets.map((e) => "${e.categoryName} -> ${e.data.entries.firstOrNull?.value.budgetAmount}").join("\n"));
 }
